e59c91993928d1ec7603ca88c07bf8096fb0cb1a
kovaletsyurii <y.kovalets@enkonix.com>
2024-02-28 15:30:13 +0200

    init

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
+const imageResize = require('gulp-image-resize');
 
 const src = 'sources/';
 const dest = 'optimized/';
 
 function optimizeImages() {
   return gulp.src([src + '*.{jpg,png}'])
+    .pipe(imageResize({
+      width: 2000,
+      upscale : false,
+      // Additional options (optional):
+      // - crop: true, // Crop to fit within dimensions
+      // - quality: 80,  // Adjust image quality (0-100)
+      // - format: 'jpg',  // Specify output format
+    }))
     .pipe(imagemin([
       imagemin.mozjpeg({quality: 75, progressive: true}),
       imagemin.optipng({optimizationLevel: 5}),
