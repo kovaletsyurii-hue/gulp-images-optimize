7cf5891fba2d94aac98c2b21a4e648076f66fc65
kovaletsyurii <y.kovalets@enkonix.com>
2024-02-28 14:59:16 +0200

    init

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+
+const src = 'sources/';
+const dest = 'optimized/';
+
+function optimizeImages() {
+  return gulp.src([src + '*.{jpg,png}'])
+    .pipe(imagemin([
+      imagemin.mozjpeg({quality: 90, progressive: true}),
+      imagemin.optipng({optimizationLevel: 5}),
+    ]))
+    .pipe(gulp.dest(dest));
+}
+
+gulp.task('default', optimizeImages);
+
+module.exports = {
+  optimizeImages
+}
